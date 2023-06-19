627ec4361a5aba6be3e7c5681ec2abe52f60fae1
LeeGwonChang <kkd2014@gmail.com>
2023-06-19 14:07:10 +0900

    2023-06-19

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import gps_msg from './src/Main';
+import test from './src/test01'
+
+export default test;
+/*{   return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); */
